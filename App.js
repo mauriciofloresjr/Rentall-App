1ac319ae18d209e4973f7f6a843bfd877563f900
Mauricio Flores Jr <floresmauricio14@gmail.com>
2021-10-20 21:32:08 -0500

    Added files after npx init

--- App.js
+++ App.js
@@ -6,45 +6,42 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import {AuthScreen} from './screens';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <AuthScreen />
-      <StatusBar style="auto" />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-
-
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ import {AuthScreen} from './screens';
+ 
+ export default function App() {
+   return (
+     <View style={styles.container}>
+       <AuthScreen />
+       <StatusBar style="auto" />
+     </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#fff',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+ });
