8c817ef9442d0ea23c3554e083bb16e5e0419949
Ryan Bracewell <89350293+rmbshsu@users.noreply.github.com>
2021-11-03 15:16:13 -0500

    Add files via upload

--- server/models/user.js
+++ server/models/user.js
@@ -9,16 +9,21 @@ const User = sequelize.define('users', {
       allowNull: false,
       primaryKey: true,
    },
+   //Server does a simple validation of Emails with form foo@bar.com
    email: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {isEmail: true}
    },
    name: {
       type: Sequelize.STRING,
    },
+   //Another validation for password, regex requires 8 characters 
+   //with at least 1 letter, 1 number, and 1 special character
    password: {
       type: Sequelize.STRING,
       allowNull: false,
+      validate: {is: "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"}
    },
 });
 
