767470de5949e03831cabcd6a231e0bb830ae5d3
Mauricio Flores Jr <floresmauricio14@gmail.com>
2021-11-06 03:09:24 -0500

    Adding and Changing all screens
    I added a bunch of screens including the main frontend screen known as "RentalScreen". Changes were made to the "AuthScreen" to make more cohesive UI. "RentalDetailsScreen" is used when looking for more detail on an item. Currently the category filter and search bar do nothing in "RentalScreen.js"

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { ImageBackground, View, Text, StyleSheet, TouchableOpacity, TextInput, Platform } from 'react-native';
 
-const API_URL = Platform.OS === 'ios' ? 'http://localhost:5000' : 'http://10.0.0.112:5000';
+const API_URL = Platform.OS === 'ios' ? 'http://localhost:5000' : 'http://10.2.3.90:5000';
+
+//Checks an attempted user password against the regular expression.
+//The regular expression calls for: At least one uppercase English letter,
+//at least one lower case English letter, at least one digit,
+//at least one special character, and at least 8 characters in length
+function passwordValidator(password) {
+    var re = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+    if (re.test(password)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 
 function AuthScreen({ navigation }) {
@@ -24,55 +37,65 @@ function AuthScreen({ navigation }) {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`, 
+                'Authorization': `Bearer ${token}`,
             },
         })
-        .then(async res => { 
-            try {
-                const jsonRes = await res.json();
-                if (res.status === 200) {
-                    setMessage(jsonRes.message);
-                }
-            } catch (err) {
+            .then(async res => {
+                try {
+                    const jsonRes = await res.json();
+                    if (res.status === 200) {
+                        setMessage(jsonRes.message);
+                    }
+                } catch (err) {
+                    console.log(err);
+                };
+            })
+            .catch(err => {
                 console.log(err);
-            };
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            });
     }
 
     const onSubmitHandler = () => {
-        const payload = {
-            email,
-            name,
-            password,
-        };
-        fetch(`${API_URL}/${isLogin ? 'login' : 'signup'}`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(payload),
-        })
-        .then(async res => { 
-            try {
-                const jsonRes = await res.json();
-                if (res.status !== 200) {
-                    setIsError(true);
-                    setMessage(jsonRes.message);
-                } else {
-                    onLoggedIn(jsonRes.token);
-                    setIsError(false);
-                    setMessage(jsonRes.message);
-                }
-            } catch (err) {
-                console.log(err);
+        //Before submiting to server, checks password with validator function
+        if (passwordValidator(password)) {
+            //a valid password was entered, generate JSON
+            const payload = {
+                email,
+                name,
+                password,
             };
-        })
-        .catch(err => {
-            console.log(err);
-        });
+            fetch(`${API_URL}/${isLogin ? 'login' : 'signup'}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload),
+            })
+                .then(async res => {
+                    try {
+                        const jsonRes = await res.json();
+                        if (res.status !== 200) {
+                            setIsError(true);
+                            setMessage(jsonRes.message);
+                        } else {
+                            onLoggedIn(jsonRes.token);
+                            setIsError(false);
+                            setMessage(jsonRes.message);
+                            navigation.navigate('Listings')
+                        }
+                    } catch (err) {
+                        console.log(err);
+                    };
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+        //an invalid password was entered, return an Error message to user
+        else {
+            setIsError(true)
+            setMessage("Invalid Password.")
+        }
     };
 
     const getMessage = () => {
@@ -81,25 +104,27 @@ function AuthScreen({ navigation }) {
     }
 
     return (
-        <ImageBackground source={require('../public/images/basic-back.png')} style={styles.image}>
+        < ImageBackground source={require('../public/images/basic-back.png')} style={styles.image} >
             <View style={styles.card}>
                 <Text style={styles.heading}>{isLogin ? 'Login' : 'Signup'}</Text>
                 <View style={styles.form}>
+
                     <View style={styles.inputs}>
                         <TextInput style={styles.input} placeholder="Email" autoCapitalize="none" onChangeText={setEmail}></TextInput>
                         {!isLogin && <TextInput style={styles.input} placeholder="Name" onChangeText={setName}></TextInput>}
                         <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" onChangeText={setPassword}></TextInput>
-                        <Text style={[styles.message, {color: isError ? 'red' : 'green'}]}>{message ? getMessage() : null}</Text>
-                        <TouchableOpacity style={styles.button} onPress={() => { onSubmitHandler; navigation.navigate('Listings') }}>
+                        <Text style={[styles.message, { color: isError ? 'red' : 'green' }]}>{message ? getMessage() : null}</Text>
+                        <TouchableOpacity style={styles.button} onPress={onSubmitHandler} >
                             <Text style={styles.buttonText}>Submit</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.buttonAlt} onPress={onChangeHandler}>
                             <Text style={styles.buttonAltText}>{isLogin ? 'Sign Up' : 'Log In'}</Text>
                         </TouchableOpacity>
-                    </View>    
+                    </View>
                 </View>
             </View>
-        </ImageBackground>    
+            <Text style={styles.buttonText}>{isLogin ? '' : ' \n Passwords must have: \n at least 8 characters \n a letter \n a number \n a special character: \n i.e. !@#$%^&*  '}</Text>
+        </ImageBackground >
     );
 };
 
@@ -108,10 +133,10 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
         alignItems: 'center',
-    },  
+    },
     card: {
         flex: 1,
-        backgroundColor: 'rgba(255, 255, 255, 0.4)',
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
         width: '80%',
         marginTop: '40%',
         borderRadius: 20,
@@ -137,18 +162,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: '10%',
-    },  
+    },
     input: {
         width: '80%',
         borderBottomWidth: 1,
         borderBottomColor: 'black',
         paddingTop: 10,
-        fontSize: 16, 
+        fontSize: 16,
         minHeight: 40,
     },
     button: {
         width: '80%',
-        backgroundColor: 'black',
+        backgroundColor: '#21C489', //changed color for visual spice
         height: 40,
         borderRadius: 50,
         justifyContent: 'center',
---
+++ screens/PaymentScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const PaymentScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>IMPLEMENT PAYMENT HERE</Text>
+        </View>
+    );
+};
+
+export default PaymentScreen;
