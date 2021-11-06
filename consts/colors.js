f63f33905010f281d0bfa70172a53fc6412d89c6
Mauricio Flores Jr <floresmauricio14@gmail.com>
2021-11-06 03:04:07 -0500

    Added colors.js
    a list of premade colors to use in the frontend

---
+++ consts/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    white: '#fff',
+    gray: '#F1F1F1',
+    black: '#000',
+    green: '#21EB7C',
+    lightGreen: '#91FF88',
+    darkGreen: '#21C489',
+    yellow: '#D19F22',
+    orange: '#D17A22',
+};
+
+export default COLORS;
