27b061ed42dc32a4e360ee67a6007ca9db0dc09e
Mauricio Flores Jr <floresmauricio14@gmail.com>
2021-11-06 03:05:17 -0500

    Updated App.js
    updated App.js to reflect changes made by me when working on frontend.

--- App.js
+++ App.js
@@ -7,41 +7,44 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {RentalScreen} from './screens';
-import {AuthScreen} from './screens';
+import { RentalScreen, AuthScreen, RentalDetailsScreen, PaymentScreen, AddRentalScreen } from './screens';
+import COLORS from './consts/colors';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="User Login" component={AuthScreen} options = {{ headerShown: false}} />
-        <Stack.Screen name="Listings" component={RentalScreen} options = {{ headerShown: false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+            <Stack.Navigator>
+                <Stack.Screen name="User Login" component={AuthScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Listings" component={RentalScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Details" component={RentalDetailsScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Add" component={AddRentalScreen} options={{ headerShown: false }} />
+                <Stack.Screen name="Payment" component={PaymentScreen} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 
