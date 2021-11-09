0e87599d35a313b9d8ccd4642c03bcdda361579d
Ryan Bracewell <89350293+rmbshsu@users.noreply.github.com>
2021-11-09 17:13:41 -0600

    Add files via upload
    Updating any changed server files

--- server/utils/database.js
+++ server/utils/database.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize('logindb', 'root', 'project', {
     dialect: 'mysql',
     host: 'localhost', 
     port: 3306,
+    pool: {
+        max: 100,
+        min: 0,
+        idle: 30000,
+        acquire: 60000,
+    },
 });
 
 export default sequelize;
