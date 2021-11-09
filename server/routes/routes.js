dfd99acfae23961e8359621ab114c9312b16b1c6
Ryan Bracewell <89350293+rmbshsu@users.noreply.github.com>
2021-11-09 17:14:31 -0600

    Add files via upload
    Update routes with additional API calls for getListings, and postListings

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 import { signup, login, isAuth } from '../controllers/auth.js';
+import {getListings, postListings} from '../controllers/rental.js';
+
+//Below is all the implemented API calls, where the /callname is the name of what is fetch()'ed
+//and the second half, is the actual function sent to the DB from the Node.js server,
+//the actual functions are defined in the controllers folder
 
 const router = express.Router();
 
@@ -10,13 +15,19 @@ router.post('/signup', signup);
 
 router.get('/private', isAuth);
 
+router.get('/getListings', getListings);
+
+router.post('/addListings', postListings);
+
 router.get('/public', (req, res, next) => {
     res.status(200).json({ message: "here is your public resource" });
 });
 
 // will match any other path
 router.use('/', (req, res, next) => {
-    res.status(404).json({error : "page not found"});
+    res.status(404).json({error : "Default path."});
 });
+
+//router.post('/addListing', addListing);
 
 export default router;
