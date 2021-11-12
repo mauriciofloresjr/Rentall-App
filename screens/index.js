5ad8badb23ff99fa370ad8b6a1bc5a0c93478219
Ryan Bracewell <89350293+rmbshsu@users.noreply.github.com>
2021-11-11 23:47:56 -0600

    Add files via upload

--- screens/index.js
+++ screens/index.js
@@ -4,9 +4,11 @@ import RentalScreen from './RentalScreen';
 import RentalDetailsScreen from './RentalDetailsScreen'
 import PaymentScreen from './PaymentScreen'
 import AddRentalScreen from './AddRentalScreen'
+import PrivacyPolicyScreen from './PrivacyPolicyScreen'
 
 export { AuthScreen };
 export { RentalScreen };
 export { AddRentalScreen };
 export { RentalDetailsScreen };
 export { PaymentScreen };
+export {PrivacyPolicyScreen};
